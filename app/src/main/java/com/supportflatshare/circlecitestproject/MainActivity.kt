333ec5f3f97ca012338464b1f1e1f5faaff489d7
Flixter <Vic_petrovski@hotmail.com>
2018-03-01 11:02:37 +0100

    Dev Branch

--- app/src/main/java/com/supportflatshare/circlecitestproject/MainActivity.kt
+++ app/src/main/java/com/supportflatshare/circlecitestproject/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action shit", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "We are currently on the dev branch!", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
 
             throw RuntimeException("Fabric Exception")
